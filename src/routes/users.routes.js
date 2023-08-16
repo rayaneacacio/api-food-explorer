5b1530316a1501b258bcd77db5e03f56ee72c236
rayaneacacio <rray74307@gmail.com>
2023-08-16 12:23:00 -0300

    criaçao da primeira rota

---
+++ src/routes/users.routes.js
@@ -0,0 +1,10 @@
+const { Router } = require("express");
+
+const UsersController = require("../controllers/users-controller");
+
+const usersRoutes = Router();
+const usersController = new UsersController();
+
+usersRoutes.post("/", usersController.create);
+
+module.exports = usersRoutes;
