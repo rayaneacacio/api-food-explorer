e2c3ca7a90a7f96c0344357d43704f3726ca7785
rayaneacacio <rray74307@gmail.com>
2023-08-18 14:37:43 -0300

    instalaçao do cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require("express-async-errors");
 require("dotenv/config");
 
 const express = require("express")
+const cors = require("cors");
 
 const AppError = require("./utils/AppError");
 const routes = require("./routes/index");
@@ -9,6 +10,7 @@ const uploadConfig = require("./configs/upload");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
 
